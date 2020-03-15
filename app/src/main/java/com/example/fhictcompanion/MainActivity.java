183810eb8ac628cf37b4a0f1777786b3e129d40c
Remy Bos <remy90@hotmail.com>
2020-03-15 18:40:04 +0100

    MAIN ACTIVITY: Add dynamic date time

--- app/src/main/java/com/example/fhictcompanion/MainActivity.java
+++ app/src/main/java/com/example/fhictcompanion/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.fhictcompanion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private static final Date TODAY = new Date();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView dateDisplay = findViewById(R.id.day_date);
+
+        SimpleDateFormat dayOfWeek = new SimpleDateFormat("EEEE");
+        SimpleDateFormat dayOfMonth = new SimpleDateFormat("d");
+        SimpleDateFormat month = new SimpleDateFormat("MMMM");
+        SimpleDateFormat year = new SimpleDateFormat("yyyy");
+
+        dateDisplay.setText("Today is " + dayOfWeek.format(TODAY) + ", " + dayOfMonth.format(TODAY) + " " + month.format(TODAY) + " " + year.format(TODAY));
     }
 }
