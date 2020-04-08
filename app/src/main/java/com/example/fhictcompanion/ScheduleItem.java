161ac92d2d45b5b7874077beee2f458104c00a11
Remy Bos <remy90@hotmail.com>
2020-04-08 15:54:18 +0200

    UNUSED CLASSES: Remove old DTOs

--- app/src/main/java/com/example/fhictcompanion/ScheduleItem.java
+++
@@ -1,9 +0,0 @@
-package com.example.fhictcompanion;
-
-public class ScheduleItem {
-    private String subject;
-    private String room;
-    private String teacher;
-    private String startsAt;
-    private String endsAt;
-}
